1b2d354097c55bc1a2b4ebea1526746a7c443beb
Elliott Cooper <ecooper@singular.health>
2024-06-23 09:21:18 +0800

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,8 +26,30 @@ subprojects {
             create<MavenPublication>(project.name) {
                 artifactId = "sdk-3cr-android"
                 group = "com.github.elliottcooper"
-                version = "1.0.22"
-                artifact("build/outputs/aar/${project.name}-debug.aar"){
+                version = "1.0.24"
+                artifact("build/outputs/aar/${project.name}-debug.aar")
+                pom {
+                    name = "@3cr/sdk-android"
+                    description = "SDK to build and deploy 3Dicom Core Renderer for Android"
+                    url = "https://docs.3cr.singular.health"
+                    licenses {
+                        license {
+                            name = "The Apache License, Version 2.0"
+                            url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                        }
+                    }
+                    developers {
+                        developer {
+                            id = "elliottcooper"
+                            name = "Elliott Cooper"
+                            email = "ecooper@singular.health"
+                        }
+                    }
+                    scm {
+                        connection = "scm:git:git://github.com/elliottcooper/sdk-3cr-android"
+                        developerConnection = "scm:git:ssh://github.com/elliottcooper/sdk-3cr-android.git"
+                        url = "https://github.com/elliottcooper/sdk-3cr-android"
+                    }
                 }
             }
         }
